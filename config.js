2fc2ab961604970a90b0db523269c792386c1d78
Balazs Hamorszky <balihb@gmail.com>
2024-02-14 12:18:45 +0100

    renovate.json5

--- config.js
+++ config.js
@@ -1,9 +1,6 @@
 // https://docs.renovatebot.com/configuration-options/
 
 module.exports = {
-  extends: [
-    "config:recommended",
-  ],
   hostRules: [
     {
       matchHost: 'https://management.azure.com',
@@ -20,43 +17,5 @@ module.exports = {
   platform: "github",
   repositories: [
     "balihb/renovate-test"
-  ],
-  customDatasources: {
-    azureWebAppPython: {
-      defaultRegistryUrlTemplate: "file://azurePythonWebAppStacks.json",
-      transformTemplates: [
-        "{\"releases\": $$.value[name=\"python\"].properties.majorVersions[value=\"3\"].minorVersions.({\"version\": $.value, \"isDeprecated\": $.stackSettings.linuxRuntimeSettings.isDeprecated})}"
-      ]
-    },
-    azureWebAppPythonHttp: {
-      defaultRegistryUrlTemplate: "https://management.azure.com/providers/Microsoft.Web/webAppStacks?stackOsType=Linux&api-version=2022-03-01",
-      transformTemplates: [
-        "{\"releases\": $$.value[name=\"python\"].properties.majorVersions[value=\"3\"].minorVersions.({\"version\": $.value, \"isDeprecated\": $.stackSettings.linuxRuntimeSettings.isDeprecated})}"
-      ]
-    }
-  },
-  customManagers: [
-    {
-      customType: "regex",
-      datasourceTemplate: "custom.azureWebAppPython",
-      depNameTemplate: "azureWebAppPython",
-      fileMatch: [
-        "\\.bicep$"
-      ],
-      matchStrings: [
-        "(?:\\/\\/\\s+renovate:(?: datasource=(?<datasource>[a-z-.]+?))?(?: depName=(?<depName>[^\\s]+?))?(?: packageName=(?<packageName>[^\\s]+?))?(?: versioning=(?<versioning>[a-z-0-9]+?))?(?: extractVersion=(?<extractVersion>[^\\s]+?))?)?\\s+linuxFxVersion: 'PYTHON\\|(?<currentValue>[0-9]\\.[0-9]+)'"
-      ]
-    },
-    {
-      customType: "regex",
-      datasourceTemplate: "custom.azureWebAppPythonHttp",
-      depNameTemplate: "azureWebAppPythonHttp",
-      fileMatch: [
-        "\\.bicep$"
-      ],
-      matchStrings: [
-        "(?:\\/\\/\\s+renovate:(?: datasource=(?<datasource>[a-z-.]+?))?(?: depName=(?<depName>[^\\s]+?))?(?: packageName=(?<packageName>[^\\s]+?))?(?: versioning=(?<versioning>[a-z-0-9]+?))?(?: extractVersion=(?<extractVersion>[^\\s]+?))?)?\\s+linuxFxVersionHttp: 'PYTHON\\|(?<currentValue>[0-9]\\.[0-9]+)'"
-      ]
-    }
   ]
 }
