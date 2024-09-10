3d2180ab94c64ce236879de08b4759cfe9176406
Balazs Hamorszky <balihb@gmail.com>
2024-09-10 16:54:52 +0200

    dash named datasource

--- config.js
+++ config.js
@@ -3,12 +3,9 @@
 module.exports = {
   hostRules: [
     {
-      matchHost: 'https://management.azure.com',
-      token: process.env.AZURE_ACCESS_TOKEN
-    },
-    {
       matchHost: 'management.azure.com',
       token: process.env.AZURE_ACCESS_TOKEN
+      // authType: ''
     }
   ],
   username: 'renovate-release',
